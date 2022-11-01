a766e9bc359ffcf10c279cb37864bc40f5bd424c
MLGEnio <97459297+MLGEnio@users.noreply.github.com>
2022-11-01 15:23:46 +0100

    Added form and validation

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,8 +1,112 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./AddUser.css";
+import CreatableSelect from "react-select/creatable";
+function AddUser(props) {
+  let initialValues = { fname: "", lname: "", email: "", hobbies: [] };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
 
-function AddUser() {
-  return <Container></Container>;
+  const changeHandler = (e) => {
+    if (Array.isArray(e)) {
+      setFormValues({ ...formValues, hobbies: e });
+    } else {
+      const { name, value } = e.target;
+      setFormValues({ ...formValues, [name]: value });
+    }
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    props.userData(formValues);
+  };
+  const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+  ];
+
+  const validate = (values) => {
+    const errors = {};
+    const regexName = /^[a-zA-Z]*$/;
+    const regexEmail =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (!values.fname) {
+      errors.fname = "First Name is required";
+    } else if (!regexName.test(values.fname)) {
+      errors.fname = "Not Valid First Name";
+    }
+    if (!values.lname) {
+      errors.lname = "Last Name is required";
+    } else if (!regexName.test(values.lname)) {
+      errors.lname = "Not valid Last Name";
+    }
+    if (!values.email) {
+      errors.email = "Email is required";
+    } else if (!regexEmail.test(values.email)) {
+      errors.email = "Not Valid Email";
+    }
+    return errors;
+  };
+  return (
+    <Form onSubmit={submitHandler}>
+      <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+      <Row className="mb-3">
+        <Form.Group as={Col} md="12">
+          <Form.Label>First name</Form.Label>
+          <input
+            name="fname"
+            className="form-control"
+            value={formValues.fname}
+            onChange={changeHandler}
+          />
+          <p>{formErrors.fname} </p>
+        </Form.Group>
+      </Row>
+      <Row>
+        <Form.Group as={Col} md="12">
+          <Form.Label>Last name</Form.Label>
+          <input
+            name="lname"
+            className="form-control"
+            value={formValues.lname}
+            onChange={changeHandler}
+          />
+          <p>{formErrors.lname} </p>
+        </Form.Group>
+      </Row>
+      <Row>
+        <Form.Group as={Col} md="12">
+          <Form.Label>Email</Form.Label>
+          <input
+            name="email"
+            className="form-control"
+            value={formValues.email}
+            onChange={changeHandler}
+          />
+          <p>{formErrors.email} </p>
+        </Form.Group>
+      </Row>
+      <Row>
+        <Form.Group as={Col} md="12">
+          <Form.Label>Hobbies</Form.Label>
+          <CreatableSelect
+            isMulti
+            options={options}
+            onChange={changeHandler}
+            name="hobbies"
+          />
+        </Form.Group>
+      </Row>
+      <Row></Row>
+
+      <Button type="submit">Submit form</Button>
+    </Form>
+  );
 }
 
 export default AddUser;
